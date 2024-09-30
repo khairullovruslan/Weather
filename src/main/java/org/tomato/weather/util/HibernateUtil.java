c4cd5b12255bc8626bc86364fe9ef25300f1cba7
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-30 12:41:15 +0300

    add util for get session factory

---
+++ src/main/java/org/tomato/weather/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package org.tomato.weather.util;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.tomato.weather.entity.Location;
+import org.tomato.weather.entity.Session;
+import org.tomato.weather.entity.User;
+
+public class HibernateUtil {
+
+    public static SessionFactory sessionFactory;
+
+
+    public static SessionFactory getSessionFactory () {
+        if (sessionFactory == null){
+            Configuration configuration = new Configuration().configure();
+            configuration.addAnnotatedClass(Location.class);
+            configuration.addAnnotatedClass(Session.class);
+            configuration.addAnnotatedClass(User.class);
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+
+
+}
