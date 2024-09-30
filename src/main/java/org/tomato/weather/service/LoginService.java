fc1838792b906acd6a0b49a9ae42eba3d4fe62e0
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-30 19:23:39 +0300

    add func for get user by login

---
+++ src/main/java/org/tomato/weather/service/LoginService.java
@@ -0,0 +1,23 @@
+package org.tomato.weather.service;
+
+import org.tomato.weather.dao.UserRepository;
+import org.tomato.weather.entity.User;
+
+import java.util.Optional;
+
+public class LoginService {
+    private LoginService(){
+    }
+    private final static LoginService INSTANCE = new LoginService();
+    private final UserRepository userRepository = UserRepository.getInstance();
+
+    public User findUserByLogin(String login){
+        Optional<User> user =  userRepository.findByLogin(login);
+        if (user.isPresent()) return user.get();
+        throw new RuntimeException();
+    }
+
+    public static LoginService getInstance() {
+        return INSTANCE;
+    }
+}
