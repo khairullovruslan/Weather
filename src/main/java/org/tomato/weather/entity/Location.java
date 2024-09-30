ba71e612b67404727320a35fcdd1fc074d54fc9e
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-30 12:18:37 +0300

    add new db model - location

---
+++ src/main/java/org/tomato/weather/entity/Location.java
@@ -0,0 +1,32 @@
+package org.tomato.weather.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Locations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+}
