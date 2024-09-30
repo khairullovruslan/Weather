98fb229f6c2bad29fc16ae1f2213c39da2f753bf
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-30 19:24:24 +0300

    add func for check that user write correct password

---
+++ src/main/java/org/tomato/weather/controller/LoginServlet.java
@@ -0,0 +1,41 @@
+package org.tomato.weather.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.tomato.weather.entity.User;
+import org.tomato.weather.service.LoginService;
+import org.tomato.weather.util.PasswordUtil;
+import org.tomato.weather.util.ThymeleafUtil;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private final LoginService loginService = LoginService.getInstance();
+    private  TemplateEngine templateEngine;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        templateEngine = (TemplateEngine) req.getServletContext().getAttribute("templateEngine");
+        WebContext context = ThymeleafUtil.buildWebContext(req, resp, getServletContext());
+        templateEngine.process("login", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("pwd");
+        User user = loginService.findUserByLogin(login);
+        if (PasswordUtil.checkPassword(password, user.getPassword())){
+            System.out.println("Welcome");
+        }
+        else {
+            System.out.println("unluck");
+        }
+
+    }
+}
