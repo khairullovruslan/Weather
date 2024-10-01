1c698b5841f0aee8cfad00d5fbaf0d950dc08705
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-01 12:11:37 +0300

    add custom query : find and update session

--- src/main/java/org/tomato/weather/dao/SessionRepository.java
+++ src/main/java/org/tomato/weather/dao/SessionRepository.java
@@ -1,10 +1,17 @@
 package org.tomato.weather.dao;
 
 import org.tomato.weather.entity.Session;
+import org.tomato.weather.entity.User;
+import org.tomato.weather.service.CookieAndSessionService;
+import org.tomato.weather.util.HibernateUtil;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
-public class SessionRepository extends BaseRepository<UUID, Session> {
+
+public class SessionRepository extends BaseRepository<String, Session> {
     private final static SessionRepository INSTANCE = new SessionRepository(Session.class);
     private SessionRepository(){
         super(Session.class);
@@ -15,6 +22,23 @@ public class SessionRepository extends BaseRepository<UUID, Session> {
     }
     private SessionRepository(Class<Session> clazz) {
         super(clazz);
+    }
+    public Optional<Session> findUpdateByUser(User user) {
+        try (var session = HibernateUtil.getSessionFactory().openSession()){
+            var transaction = session.beginTransaction();
+            var cb = session.getCriteriaBuilder();
+            var criteria = cb.createQuery(Session.class);
+            var sessionEnt = criteria.from(Session.class);
+            criteria.select(sessionEnt).where(cb.equal(sessionEnt.get("user").get("id"), user.getId()));
+            Session session1 = session.createQuery(criteria).uniqueResult();
+            if (session1 != null){
+                session1.setExpiresAt(LocalDate.now().atStartOfDay()
+                        .plusHours(CookieAndSessionService.hoursLiveSession));
+                session.merge(session1);
+            }
+            transaction.commit();
+            return Optional.ofNullable(session1);
+        }
     }
 
 }
