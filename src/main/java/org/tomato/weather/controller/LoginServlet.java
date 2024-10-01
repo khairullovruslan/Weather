0e6b2c3500467df4291fc61e8e5bf061acb243e3
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-01 12:10:07 +0300

    added error handler

--- src/main/java/org/tomato/weather/controller/LoginServlet.java
+++ src/main/java/org/tomato/weather/controller/LoginServlet.java
@@ -9,12 +9,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.tomato.weather.entity.User;
+import org.tomato.weather.exception.SessionDuplicateException;
 import org.tomato.weather.service.CookieAndSessionService;
 import org.tomato.weather.service.LoginService;
 import org.tomato.weather.util.PasswordUtil;
 import org.tomato.weather.util.ThymeleafUtil;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -34,7 +36,21 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("pwd");
         User user = loginService.findUserByLogin(login);
         if (PasswordUtil.checkPassword(password, user.getPassword())){
-            Cookie cookie = cookieAndSessionService.createCookieAndRegisterNewSession(user);
+            Cookie cookie;
+            try {
+                cookie = cookieAndSessionService.createCookieAndRegisterNewSession(user);
+            }
+            catch (SessionDuplicateException e){
+
+                Optional<Cookie> cookieOptional = cookieAndSessionService.findAndUpdateSessionByUserId(user);
+                if (cookieOptional.isPresent()){
+                    cookie = cookieOptional.get();
+                }
+                else {
+                    resp.sendRedirect(req.getContextPath() + "/login");
+                    return;
+                }
+            }
             cookie.setMaxAge(60 * 60 * 2);
             resp.addCookie(cookie);
         }
