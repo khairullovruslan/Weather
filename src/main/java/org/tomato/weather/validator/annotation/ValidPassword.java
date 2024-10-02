6e0d64d54729fa93c3c7877a0553a535b17c8017
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-02 14:38:54 +0300

    feat: created annotation for validation

---
+++ src/main/java/org/tomato/weather/validator/annotation/ValidPassword.java
@@ -0,0 +1,22 @@
+package org.tomato.weather.validator.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import org.tomato.weather.validator.PasswordValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+
+    String message() default "Пароль не соответствует требованиям.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
