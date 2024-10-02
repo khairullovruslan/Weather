6a81f0de9934b2f800252381700352d35bab17f9
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-02 14:50:50 +0300

    feat: added annotation for loginField

--- src/main/java/org/tomato/weather/dto/UserRegistrationDto.java
+++ src/main/java/org/tomato/weather/dto/UserRegistrationDto.java
@@ -1,15 +1,20 @@
 package org.tomato.weather.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
+import org.tomato.weather.validator.annotation.ValidLogin;
 import org.tomato.weather.validator.annotation.ValidPassword;
 
 @Builder
 @Data
 public class UserRegistrationDto {
 
+    @ValidLogin
+    @NotNull
     private String login;
 
     @ValidPassword
+    @NotNull
     private String password;
 }
