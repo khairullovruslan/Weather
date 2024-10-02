ad1e2424127c0c117179189a5f555bdad0ea12b2
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-02 15:13:10 +0300

    feat: added func login

--- src/main/java/org/tomato/weather/service/AuthService.java
+++ src/main/java/org/tomato/weather/service/AuthService.java
@@ -1,35 +1,52 @@
 package org.tomato.weather.service;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.hibernate.exception.ConstraintViolationException;
 import org.tomato.weather.dao.SessionRepository;
 import org.tomato.weather.dao.UserRepository;
-import org.tomato.weather.dto.UserDto;
 import org.tomato.weather.dto.UserRegistrationDto;
 import org.tomato.weather.entity.User;
 import org.tomato.weather.exception.LoginDuplicateException;
+import org.tomato.weather.exception.SessionDuplicateException;
+import org.tomato.weather.exception.WrongPasswordException;
+import org.tomato.weather.util.PasswordUtil;
 
 public class AuthService {
-    private final static  AuthService INSTANCE = new AuthService();
+    private final static AuthService INSTANCE = new AuthService();
 
     private final UserRepository userRepository = UserRepository.getInstance();
     private final SessionRepository sessionRepository = SessionRepository.getInstance();
-
+    private final LoginService loginService = LoginService.getInstance();
+    private final CookieAndSessionService cookieAndSessionService = CookieAndSessionService.getInstance();
 
     public static AuthService getInstance() {
         return INSTANCE;
     }
 
-    public User registration(UserRegistrationDto user){
+    public User registration(UserRegistrationDto user) {
         try {
-            return  userRepository.save(User.builder()
+            return userRepository.save(User.builder()
                     .login(user.getLogin())
                     .password(user.getPassword())
                     .build());
-        }
-        catch (ConstraintViolationException e){
+        } catch (ConstraintViolationException e) {
             throw new LoginDuplicateException();
         }
+    }
 
+    public Cookie login(HttpServletRequest req) {
+        String login = req.getParameter("login");
+        String password = req.getParameter("pwd");
+        User user = loginService.findUserByLogin(login);
+        if (PasswordUtil.checkPassword(password, user.getPassword())) {
+            try {
+                return cookieAndSessionService.createCookieAndRegisterNewSession(user);
+            } catch (SessionDuplicateException e) {
+                return cookieAndSessionService.findAndUpdateSessionByUserId(user);
+            }
+        }
+        throw new WrongPasswordException();
     }
 
     public void logout(String sessionId) {
