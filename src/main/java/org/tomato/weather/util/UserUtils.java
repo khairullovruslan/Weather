a0ebb580e476d2e4c5b5ef4112c29140babfa053
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-02 14:38:24 +0300

    feat: created mapper req -> UserRegDTO

---
+++ src/main/java/org/tomato/weather/util/UserUtils.java
@@ -0,0 +1,16 @@
+package org.tomato.weather.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.tomato.weather.dto.UserRegistrationDto;
+
+public class UserUtils {
+    public static UserRegistrationDto buildUserRegistrationDto(HttpServletRequest req){
+        String login = req.getParameter("login");
+        String pwd = req.getParameter("pwd");
+        return UserRegistrationDto
+                .builder()
+                .password(pwd)
+                .login(login)
+                .build();
+    }
+}
