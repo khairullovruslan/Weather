8463ad016b393841368c8514fbf4f800445186c3
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-02 11:52:49 +0300

    feat: added function to delete completed sessions

--- src/main/java/org/tomato/weather/dao/SessionRepository.java
+++ src/main/java/org/tomato/weather/dao/SessionRepository.java
@@ -1,5 +1,12 @@
 package org.tomato.weather.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaDelete;
+import jakarta.persistence.criteria.Root;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tomato.weather.entity.Session;
 import org.tomato.weather.entity.User;
 import org.tomato.weather.service.CookieAndSessionService;
@@ -8,10 +15,11 @@ import org.tomato.weather.util.HibernateUtil;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
-import java.util.UUID;
 
 
+@Slf4j
 public class SessionRepository extends BaseRepository<String, Session> {
+    private static final Logger logger =  LoggerFactory.getLogger(SessionRepository.class);
     private final static SessionRepository INSTANCE = new SessionRepository(Session.class);
     private SessionRepository(){
         super(Session.class);
@@ -41,4 +49,16 @@ public class SessionRepository extends BaseRepository<String, Session> {
         }
     }
 
+    public void cleanSession() {
+        try (var session = HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaDelete<Session> criteriaDelete = cb.createCriteriaDelete(Session.class);
+            Root<Session> sessionEntity = criteriaDelete.from(Session.class);
+            criteriaDelete.where(cb.lessThan(sessionEntity.get("expiresAt"), LocalDateTime.now()));
+            int count = session.createQuery(criteriaDelete).executeUpdate();
+            logger.info("Count delete session {}", count);
+            transaction.commit();
+        }
+    }
 }
