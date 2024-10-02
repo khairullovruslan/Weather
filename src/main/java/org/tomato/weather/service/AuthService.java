b9c4bff315a47e40d8fa936caa563cf77af5fadc
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-02 11:46:48 +0300

    feat: throw LoginDuplicateException when login is exists in db

--- src/main/java/org/tomato/weather/service/AuthService.java
+++ src/main/java/org/tomato/weather/service/AuthService.java
@@ -1,10 +1,14 @@
 package org.tomato.weather.service;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.exception.ConstraintViolationException;
+import org.postgresql.util.PSQLException;
 import org.tomato.weather.dao.SessionRepository;
 import org.tomato.weather.dao.UserRepository;
 import org.tomato.weather.dto.UserDto;
 import org.tomato.weather.entity.User;
+import org.tomato.weather.exception.LoginDuplicateException;
+import org.tomato.weather.exception.SessionDuplicateException;
 import org.tomato.weather.mappers.UserMapper;
 
 import java.util.UUID;
@@ -21,10 +25,16 @@ public class AuthService {
     }
 
     public User registration(UserDto user){
-        return userRepository.save(User.builder()
-                        .login(user.login())
-                        .password(user.password())
-                .build());
+        try {
+            return  userRepository.save(User.builder()
+                    .login(user.login())
+                    .password(user.password())
+                    .build());
+        }
+        catch (ConstraintViolationException e){
+            throw new LoginDuplicateException();
+        }
+
     }
 
     public void logout(String sessionId) {
