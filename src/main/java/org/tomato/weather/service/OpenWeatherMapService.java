5ca23f64cbfec43af3ccd3e33747d2fad1e82d32
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-07 11:32:47 +0300

    feat: added func for getWeatherList

--- src/main/java/org/tomato/weather/service/OpenWeatherMapService.java
+++ src/main/java/org/tomato/weather/service/OpenWeatherMapService.java
@@ -16,15 +16,16 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OpenWeatherMapService {
     private final HttpClient httpClient = HttpClient.newHttpClient();
-    private final  ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private final String KEY = System.getenv("api_key");
-    private static final String HTTP_REQUEST_GET_CITY_BY_NAME = "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s&lang=en&units=metric";
+    private static final String HTTP_REQUEST_GET_CITY_BY_NAME = "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=5&appid=%s";
     private static final String HTTP_REQUEST_GET_WEATHER = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&lang=en&units=metric";
-
 
     public WeatherDTO getWeather(Location location) throws URISyntaxException, IOException, InterruptedException {
         String cityReq = String.format(HTTP_REQUEST_GET_WEATHER, location.getLatitude(), location.getLongitude(), KEY);
@@ -35,6 +36,20 @@ public class OpenWeatherMapService {
             return weatherDTO;
         }
         throw new WeatherNotFoundException();
+    }
+
+    public Map<LocationDTO, WeatherDTO> getWeatherList(List<LocationDTO> locations) throws URISyntaxException, IOException, InterruptedException {
+        Map<LocationDTO, WeatherDTO> map = new HashMap<>();
+        for (LocationDTO location : locations) {
+            String cityReq = String.format(HTTP_REQUEST_GET_WEATHER, location.getLat(), location.getLon(), KEY);
+            HttpResponse<String> response = httpSender(cityReq);
+            if (response.statusCode() == 200) {
+                WeatherDTO weatherDTO = objectMapper.readValue(response.body(), WeatherDTO.class);
+                weatherDTO.setName(location.getName());
+                map.put(location, weatherDTO);
+            }
+        }
+        return map;
     }
 
     public List<LocationDTO> getCityByName(String name) throws URISyntaxException, IOException, InterruptedException {
