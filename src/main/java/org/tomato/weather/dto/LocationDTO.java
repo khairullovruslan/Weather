35766937b76646fc0871ac8cd407d190eb9bc7e3
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-07 11:22:08 +0300

    feat: override hashCode and equals

--- src/main/java/org/tomato/weather/dto/LocationDTO.java
+++ src/main/java/org/tomato/weather/dto/LocationDTO.java
@@ -9,6 +9,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @ToString
 @Getter
 @Setter
@@ -22,10 +24,24 @@ public class LocationDTO {
     @NotNull
     private double lat;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LocationDTO that = (LocationDTO) o;
+        return Double.compare(lat, that.lat) == 0 && Double.compare(lon, that.lon) == 0 && Objects.equals(name, that.name) && Objects.equals(country, that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lat, lon, country);
+    }
+
     @DecimalMax(value = "180.0")
     @DecimalMin(value = "-180.0")
     @NotNull
     private double lon;
 
     private String country;
+    private String state;
 }
