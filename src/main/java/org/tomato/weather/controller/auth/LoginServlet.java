d1fa43495faa0ae1ed601e1a18e9cb470c350552
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-07 12:15:49 +0300

    refactor: added redirect to home page

--- src/main/java/org/tomato/weather/controller/auth/LoginServlet.java
+++ src/main/java/org/tomato/weather/controller/auth/LoginServlet.java
@@ -1,22 +1,12 @@
 package org.tomato.weather.controller.auth;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.thymeleaf.context.WebContext;
 import org.tomato.weather.controller.BaseServlet;
-import org.tomato.weather.entity.User;
-import org.tomato.weather.exception.SessionDuplicateException;
 import org.tomato.weather.service.AuthService;
-import org.tomato.weather.service.CookieAndSessionService;
-import org.tomato.weather.service.LoginService;
-import org.tomato.weather.util.PasswordUtil;
-import org.tomato.weather.util.ThymeleafUtil;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @WebServlet("/login")
 public class LoginServlet extends BaseServlet {
@@ -24,16 +14,14 @@ public class LoginServlet extends BaseServlet {
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         processTemplate("login", req, resp);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.addCookie(authService.login(req));
-        // todo  redirect home page
-
-
+        resp.sendRedirect(req.getContextPath() + "/home");
 
     }
 }
