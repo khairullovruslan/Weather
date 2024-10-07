91384d69beb71eca50ae8ae00387662e62c06a40
khairullovruslan <khairullov.ruslan@gmail.com>
2024-10-07 11:19:20 +0300

    feat: created custom exception handler for views with ajax

---
+++ src/main/java/org/tomato/weather/util/AjaxExceptionHandler.java
@@ -0,0 +1,44 @@
+package org.tomato.weather.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolation;
+import lombok.SneakyThrows;
+import org.tomato.weather.exception.*;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AjaxExceptionHandler {
+    private static final AjaxExceptionHandler INSTANCE = new AjaxExceptionHandler();
+    private AjaxExceptionHandler(){}
+
+    public static AjaxExceptionHandler getInstance() {
+        return INSTANCE;
+    }
+
+    @SneakyThrows
+    public void senderRespUrl(String url, HttpServletResponse resp){
+        resp.setContentType("application/json");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter out = resp.getWriter();
+        out.print("{\"url\": \"" + url + "\"}");
+        out.flush();
+    }
+    public void handle(Exception e, HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            switch (e) {
+                case SessionNotFoundException ignored -> {
+                    senderRespUrl(req.getContextPath() + "/login", resp);
+                }
+
+                default -> senderRespUrl(req.getContextPath() + "/error", resp);
+            }
+        } catch (Exception notFoundException) {
+            notFoundException.printStackTrace();
+            senderRespUrl(req.getContextPath() + "/error", resp);
+        }
+
+    }
+}
